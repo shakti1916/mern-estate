dc47b2a775560cd8aef8e92c58c39ee8904c6562
shakti1916 <shaktimaddeshiya34321@gmail.com>
2024-05-02 00:15:49 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from "express"
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config()
 
 const app = express();
 
-mongoose.connect("mongodb+srv://shakti:shakti@cluster0.lgewaz0.mongodb.net/mern-estate")
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('connected to mongo')
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000!!')
