094c0d5162403325fda1d8d9c2916e93bf6dd4d1
shakti1916 <shaktimaddeshiya34321@gmail.com>
2024-05-10 00:32:49 +0530

    createad api for user update

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import userRouter from "../api/routes/user-route.js"
 import authRouter from "../api/routes/auth-route.js"
+import cookieParser from "cookie-parser";
 
 
 
@@ -11,6 +12,7 @@ dotenv.config()
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('connected to mongo')
